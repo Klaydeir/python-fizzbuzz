eb16b50bd83796f984999cdcf84304af78ce29f7
kadoOo <hkadir427@gmail.com>
2023-10-26 23:38:29 +0300

    FizzBuzz - Üç ve Beşin Katları
    Kullanıcıdan alınan değer uzunluktaki bir dizi içindeki üçün katları için "Fizz", beşin katları için "Buzz", hem beş hem de üçün katları için "FizzBuzz", her üçü de değilse sayının kendisini yazdırır.

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return str(n)
+
+length = int(input("Length of array: "))
+
+for i in range(1, length+1):
+    print(fizzbuzz(i))
